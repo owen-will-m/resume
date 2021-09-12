06f28f78cbec419bb36dded745496271bfe20aef
Will Owen <will@Wills-MacBook-Air.local>
2021-09-12 17:13:41 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import img from "./assets/plants.png";
+import profile from "./assets/prof.png";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+this.state = { activePage: "About"};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <div className="subheader">
+            <div className="image"> <img src={profile}></img></div>
+            <div className="title">
+              <h2>Will Owen {this.x} </h2>
+                UX Designer / Web Developer<br></br>Providence, RI
+
+            </div>
+            <div className="image2">
+              <img src={img}></img>
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        <div className="body">
+          <div className="sidebar">
+            <div className="item" onclick={function(){alert();}}>About</div>
+            <div className="item">Projects</div>
+            <div className="item">Resume</div>
+            <div className="item">Contact</div>
+          </div>
+          <div className="window"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
